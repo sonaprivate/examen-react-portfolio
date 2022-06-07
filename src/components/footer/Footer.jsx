187067f57ff1b80582b153f8954562d7f02874ed
suay-selay <suay_999@hotmail.com>
2022-06-07 14:52:53 +0200

    Footer is done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,21 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Sona Rahimova</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer_copyright'>
+        <small>&copy; Sona Rahimova Portfolio. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
